5e7007c79bf8b04ac8bfe496f34922f2d535efdb
MaxCoder-mnnit <96427158+MaxCoder-mnnit@users.noreply.github.com>
2022-04-24 09:36:58 +0530

    Delete models/public directory

--- models/public/js/javascript.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navbar-fixed-top");
-      $nav.toggleClass( 'scrolled', $( this ).scrollTop() > $nav.height() );
-      
-  });
-});
